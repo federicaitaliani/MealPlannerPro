5c9a71d55e5d61687c8994fc1713e01498671286
sarahabbo <134351802+sarahabbo@users.noreply.github.com>
2024-12-07 19:45:22 -0500

    Update generate_meal_plan.php

--- generate_meal_plan.php
+++ generate_meal_plan.php
@@ -1,28 +1,8 @@
+
+
 <?php
-// Include your database connection
-include 'db.php';
-header('Content-Type: application/json');
-
-// API Key
-$apiKey = "fbd430d48e5a4d1bbe7dbb6190c3b135";
-
-// Get user input
-$preferences = $_POST['preferences'] ?? 'any';
-$calories = $_POST['calories'] ?? 2000;
-$budget = $_POST['budget'] ?? 50;
-
-// Meal calorie allocation
-$mealCalories = [
-    'breakfast' => intval($calories * 0.25),
-    'lunch' => intval($calories * 0.35),
-    'dinner' => intval($calories * 0.40),
-];
-
-$mealPlan = [];
-$debugInfo = []; // Store debug data here
-
 foreach ($mealCalories as $mealType => $calorieLimit) {
-    // Build the API URL
+    // Build the API URL for meal search
     $url = "https://api.spoonacular.com/recipes/complexSearch?apiKey=$apiKey&diet=$preferences&type=$mealType&maxCalories=$calorieLimit&addRecipeInformation=true&sort=random&number=1";
 
     // Make API request
@@ -32,21 +12,33 @@ foreach ($mealCalories as $mealType => $calorieLimit) {
     $response = curl_exec($ch);
     curl_close($ch);
 
-    // Debug information
-    $debugInfo[$mealType] = [
-        'url' => $url,
-        'response' => $response,
-    ];
-
     $data = json_decode($response, true);
 
     if (isset($data['results'][0])) {
         $recipe = $data['results'][0];
+        $recipeId = $recipe['id'];  // Get the recipe ID for detailed fetching
+
+        // Fetch detailed recipe info
+        $recipeDetailUrl = "https://api.spoonacular.com/recipes/$recipeId/information?apiKey=$apiKey";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $recipeDetailUrl);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $recipeDetailsResponse = curl_exec($ch);
+        curl_close($ch);
+
+        $recipeDetails = json_decode($recipeDetailsResponse, true);
+
+        // Extract ingredients and instructions
+        $ingredients = $recipeDetails['extendedIngredients'];
+        $instructions = $recipeDetails['instructions'];
+
         $mealPlan[$mealType] = [
             'title' => $recipe['title'],
             'calories' => $recipe['nutrition']['nutrients'][0]['amount'] ?? 'Unknown',
             'image' => $recipe['image'],
-            'link' => "https://spoonacular.com/recipes/" . $recipe['title'] . "-" . $recipe['id'],
+            'recipeId' => $recipeId, // Pass the recipe ID to use on the recipe page
+            'ingredients' => $ingredients,
+            'instructions' => $instructions,
         ];
     } else {
         $mealPlan[$mealType] = [
@@ -57,12 +49,74 @@ foreach ($mealCalories as $mealType => $calorieLimit) {
         ];
     }
 }
+ ?>
 
-// Return the meal plan along with debug info
-echo json_encode([
-    'success' => true,
-    'mealPlan' => $mealPlan,
-    'debug' => $debugInfo,
-]);
-exit();
-?>
+
+<!-- 
+// // Include your database connection
+// include 'db.php';
+// header('Content-Type: application/json');
+
+// // API Key
+// $apiKey = "fbd430d48e5a4d1bbe7dbb6190c3b135";
+
+// // Get user input
+// $preferences = $_POST['preferences'] ?? 'any';
+// $calories = $_POST['calories'] ?? 2000;
+// $budget = $_POST['budget'] ?? 50;
+
+// // Meal calorie allocation
+// $mealCalories = [
+//     'breakfast' => intval($calories * 0.25),
+//     'lunch' => intval($calories * 0.35),
+//     'dinner' => intval($calories * 0.40),
+// ];
+
+// $mealPlan = [];
+// $debugInfo = []; // Store debug data here
+
+// foreach ($mealCalories as $mealType => $calorieLimit) {
+//     // Build the API URL
+//     $url = "https://api.spoonacular.com/recipes/complexSearch?apiKey=$apiKey&diet=$preferences&type=$mealType&maxCalories=$calorieLimit&addRecipeInformation=true&sort=random&number=1";
+
+//     // Make API request
+//     $ch = curl_init();
+//     curl_setopt($ch, CURLOPT_URL, $url);
+//     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+//     $response = curl_exec($ch);
+//     curl_close($ch);
+
+//     // Debug information
+//     $debugInfo[$mealType] = [
+//         'url' => $url,
+//         'response' => $response,
+//     ];
+
+//     $data = json_decode($response, true);
+
+//     if (isset($data['results'][0])) {
+//         $recipe = $data['results'][0];
+//         $mealPlan[$mealType] = [
+//             'title' => $recipe['title'],
+//             'calories' => $recipe['nutrition']['nutrients'][0]['amount'] ?? 'Unknown',
+//             'image' => $recipe['image'],
+//             'link' => "https://spoonacular.com/recipes/" . $recipe['title'] . "-" . $recipe['id'],
+//         ];
+//     } else {
+//         $mealPlan[$mealType] = [
+//             'title' => 'No recipe found',
+//             'calories' => 'N/A',
+//             'image' => 'https://via.placeholder.com/150',
+//             'link' => '#',
+//         ];
+//     }
+// }
+
+// // Return the meal plan along with debug info
+// echo json_encode([
+//     'success' => true,
+//     'mealPlan' => $mealPlan,
+//     'debug' => $debugInfo,
+// ]);
+// exit();
+ -->
